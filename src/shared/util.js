9ab9c203d8b0b9d93a6f54e06ff87bf69073abfe
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-03-12 21:44:26 +0900

    Cosme

--- src/shared/util.js
+++ src/shared/util.js
@@ -1,13 +1,13 @@
 window.Util = {
-  renderCanvas(canvas) {
-    const ctx = canvas.getContext('2d');
+  renderCanvas(canvasEl) {
+    const ctx = canvasEl.getContext('2d');
 
     setInterval(() => {
       const r = Math.floor(Math.random() * 255);
       const g = Math.floor(Math.random() * 255);
       const b = Math.floor(Math.random() * 255);
       ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-      ctx.fillRect(0, 0, canvas.width, canvas. height);
+      ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
     }, 500);
   }
 };

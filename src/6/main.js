3fce30f2cb40e910d547caacdc56d8920a5273c2
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-03-15 15:04:23 +0900

    Test sfu room

---
+++ src/6/main.js
@@ -0,0 +1,42 @@
+const randId = Date.now();
+const peer1 = new Peer({
+  key: window.__SKYWAY_KEY__,
+});
+const peer2 = new Peer({
+  key: window.__SKYWAY_KEY__,
+  config: {
+    iceTransportPolicy: 'relay',
+  },
+});
+
+Util.renderCanvas(Util.$('#local-canvas'));
+Util.renderCanvas(Util.$('#remote-canvas'));
+Util.$('#local-join-btn').onclick = localJoin;
+Util.$('#remote-join-btn').onclick = remoteJoin;
+Util.$('#remote-leave-btn').onclick = remoteLeave;
+
+function localJoin() {
+  const stream = Util.$('#local-canvas').captureStream();
+  const sfuRoomL = peer1.joinRoom(randId, { stream, mode: 'sfu' });
+
+  sfuRoomL.on('stream', stream => {
+    Util.$('#local-video').srcObject = stream;
+  });
+  sfuRoomL.on('peerLeave', () => {
+    console.log('remote peer leave');
+  });
+}
+
+let sfuRoomR;
+function remoteJoin() {
+  const stream = Util.$('#remote-canvas').captureStream();
+  sfuRoomR = peer2.joinRoom(randId, { stream, mode: 'sfu' });
+
+  sfuRoomR.on('stream', stream => {
+    Util.$('#remote-video').srcObject = stream;
+  });
+}
+
+function remoteLeave() {
+  sfuRoomR.close();
+}
---
+++ tests/6.js
@@ -0,0 +1,66 @@
+import { Selector, ClientFunction } from 'testcafe';
+import { serverUrl, sleepForFirefoxHeadless } from './shared/const';
+
+fixture('SkyWay SfuRoom')
+  .page(`${serverUrl}/6`);
+
+const localJoinBtn = Selector('#local-join-btn');
+const remoteJoinBtn = Selector('#remote-join-btn');
+const remoteLeaveBtn = Selector('#remote-leave-btn');
+
+test('should join room', async t => {
+  // get black frame
+  const l1 = await ClientFunction(() => Util.getVideoColorAsDataURL('#local-video'));
+  const r1 = await ClientFunction(() => Util.getVideoColorAsDataURL('#remote-video'));
+
+  // click to join room
+  await t.click(localJoinBtn);
+  await t.click(remoteJoinBtn);
+  // XXX: need to wait for firefox:headless...
+  await t.wait(sleepForFirefoxHeadless);
+
+  // if connected, video is not black now
+  const l2 = await ClientFunction(() => Util.getVideoColorAsDataURL('#local-video'));
+  const r2 = await ClientFunction(() => Util.getVideoColorAsDataURL('#remote-video'));
+  await t.expect(l1).notEql(l2);
+  await t.expect(r1).notEql(r2);
+});
+
+test('should leave', async t => {
+  // click to start call
+  await t.click(localJoinBtn);
+  await t.click(remoteJoinBtn);
+  // XXX: need to wait for firefox:headless...
+  await t.wait(sleepForFirefoxHeadless);
+
+  // then leave
+  await t.click(remoteLeaveBtn);
+
+  // leave log printed
+  const { log } = await t.getBrowserConsoleMessages();
+  await t.expect(log.length).eql(1);
+});
+
+test('should rejoin after leave', async t => {
+  // click to start call
+  await t.click(localJoinBtn);
+  await t.click(remoteJoinBtn);
+  // XXX: need to wait for firefox:headless...
+  await t.wait(sleepForFirefoxHeadless);
+
+  // then leave
+  await t.click(remoteLeaveBtn);
+
+  const l1 = await ClientFunction(() => Util.getVideoColorAsDataURL('#local-video'));
+  const r1 = await ClientFunction(() => Util.getVideoColorAsDataURL('#remote-video'));
+
+  await t.click(remoteJoinBtn);
+  // XXX: need to wait for firefox:headless...
+  await t.wait(sleepForFirefoxHeadless);
+
+  // if rejoined, video will re-render
+  const l2 = await ClientFunction(() => Util.getVideoColorAsDataURL('#local-video'));
+  const r2 = await ClientFunction(() => Util.getVideoColorAsDataURL('#remote-video'));
+  await t.expect(l1).notEql(l2);
+  await t.expect(r1).notEql(r2);
+});

07d03e1210f2f65d3a9e0b167afb5d83def64495
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-04-06 13:57:59 +0900

    On for replace again

--- src/4/main.js
+++ src/4/main.js
@@ -11,7 +11,7 @@ const peer2 = new Peer(randId, {
 
 peer2.on('call', conn => {
   conn.answer();
-  conn.once('stream', stream => {
+  conn.on('stream', stream => {
     Util.$('#remote-video').srcObject = stream;
   });
 });

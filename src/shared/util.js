2016f51e5d1f681c63a7049bbe624d116f6ad68e
Yuji Sugiura <y.sugiura.0316@gmail.com>
2018-04-06 13:23:12 +0900

    Add getCanvasMediaStream()

--- src/shared/util.js
+++ src/shared/util.js
@@ -4,10 +4,11 @@ window.Util = {
   },
   getVideoColorAsDataURL(selector) {
     const video = document.querySelector(selector);
-
     const canvas = document.createElement('canvas');
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0);
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    canvas.getContext('2d').drawImage(video, 0, 0);
     return canvas.toDataURL();
   },
   async getMediaStream() {
@@ -18,5 +19,18 @@ window.Util = {
       fake: 1,
     });
     return stream;
+  },
+  getCanvasMediaStream() {
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    setInterval(() => {
+      const r = Math.floor(Math.random() * 255);
+      const g = Math.floor(Math.random() * 255);
+      const b = Math.floor(Math.random() * 255);
+      ctx.fillColor = `rgb(${r}, ${g}, ${b})`;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+    }, 500);
+
+    return canvas.captureStream();
   },
 };
